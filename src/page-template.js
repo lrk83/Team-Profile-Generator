9b08b731b101dc2d5894a2f9666653873763a1c4
Lukas Keel <lrk83@cornell.edu>
2021-07-14 15:45:38 -0400

    added bass inquirer questions

---
+++ src/page-template.js
@@ -0,0 +1,33 @@
+const engineer= require("../lib/Engineer");
+const intern= require("../lib/Intern");
+const manager= require("../lib/Manager");
+
+const generatePage = templateData => {
+    console.log(templateData);
+
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+      
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Team Profile Example</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+      <header>
+        <h1>My Team<h1>
+      </header>
+      <main class="container my-5">
+      </main>
+    </body>
+    </html>
+    `;
+};
+
+module.exports = generatePage;
